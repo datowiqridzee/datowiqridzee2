b51ee7341010e73b0b71b7e42042dd29f803188c
datowiqridze\goa <wiqridzed@glail.com>
2024-09-27 12:17:28 -0700

    commit

---
+++ day 40/classwork/codewars/codewars .py
@@ -0,0 +1,13 @@
+def capitalize(s):
+    even_capitalized = ""
+    odd_capitalized = ""
+    
+    for index in range(len(s)):
+        if index % 2 == 0:
+            even_capitalized += s[index].upper()
+            odd_capitalized += s[index]
+        else:
+            even_capitalized += s[index]
+            odd_capitalized += s[index].upper()
+    
+    return [even_capitalized, odd_capitalized]
